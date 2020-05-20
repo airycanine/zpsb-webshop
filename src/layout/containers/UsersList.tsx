cdeb555df0de8b506e1d9226fbaeac3d1352f4af
qbavv <kubawalat@gmail.com>
2020-05-20 23:17:53 +0200

    button styling in UsersList limited to td

--- src/layout/containers/UsersList.tsx
+++ src/layout/containers/UsersList.tsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Reducers } from "../../store/reducers/reducers";
 import { Customer } from "../../interfaces/CustomerInfo";
 import { AdminActionsDispatcher } from "../../store/dispatchers/admin/AdminActionsDispatcher";
 import "../../styles/usersList.css";
+import { Car } from "../../interfaces/CarInfo";
+import VerticallyCenteredModal from "../components/verticallyCenteredModal";
+import { CarActionsDispatcher } from "../../store/dispatchers/car/CarActionsDispatcher";
 
 interface PropsFromStore {
   customers: Customer[];
   roles: string[];
   loggedCustomer: Customer;
+  cars: Car[];
 }
 const UsersList = () => {
   const adminActionsDispatcher = new AdminActionsDispatcher(useDispatch());
-  const { customers, roles, loggedCustomer } = useSelector<
+  const carActionsDispatcher = new CarActionsDispatcher(useDispatch());
+  const { customers, roles, loggedCustomer, cars } = useSelector<
     Reducers,
     PropsFromStore
   >((state: Reducers) => {
@@ -21,56 +26,106 @@ const UsersList = () => {
       customers: state.adminReducer.customers,
       roles: state.customerReducer.roles,
       loggedCustomer: state.customerReducer.customer,
+      cars: state.carsReducer.cars,
     };
   });
+
+  const [selectedOffer, setSelectedOffer] = useState<Car>({
+    brand: "",
+    buyer: undefined,
+    currency: "",
+    images: [],
+    licenceNumber: "",
+    model: "",
+    price: 0,
+    seller: undefined,
+  });
+  const [modalShown, setModalShown] = useState(false);
+
   useEffect(() => {
     adminActionsDispatcher.getCustomers();
   }, []);
-  console.log(customers);
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Email</th>
-          <th>Active offers</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Street</th>
-          <th>City</th>
-          <th>Zip-code</th>
-          <th>Voivodeship</th>
-          <th>Kill</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customers
-          .filter((customer) => customer.email !== loggedCustomer.email)
-          .map((customer, index) => (
-            <tr>
-              <td>{index + 1}</td>
-              <td>{customer.email}</td>
-              <td>offers</td>
-              <td>{customer.firstName}</td>
-              <td>{customer.lastName}</td>
-              <td>{customer.address.street}</td>
-              <td>{customer.address.city}</td>
-              <td>{customer.address.zip}</td>
-              <td>{customer.address.voivodeship}</td>
-              <td>
-                <Button
-                  variant="danger"
-                  onClick={() => {
-                    adminActionsDispatcher.deleteCustomer(customer);
-                  }}
-                >
-                  Remove user
-                </Button>
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </Table>
+    <>
+      <VerticallyCenteredModal
+        title={"User offer"}
+        show={modalShown}
+        onHide={() => setModalShown(false)}
+        size="sm"
+      >
+        <div className="text-center">
+          Delete car with regplate {selectedOffer?.licenceNumber}?
+        </div>
+        <Button
+          onClick={() => {
+            carActionsDispatcher.deleteCar(selectedOffer);
+            setModalShown(false);
+          }}
+        >
+          YES
+        </Button>
+      </VerticallyCenteredModal>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th>Active offers</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>Zip-code</th>
+            <th>Voivodeship</th>
+            <th>Kill</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers
+            .filter((customer) => customer.email !== loggedCustomer.email)
+            .map((customer, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{customer.email}</td>
+                <td>
+                  {cars
+                    .filter((car) => car.seller === customer.email)
+                    .map((car) => (
+                      <tr className="text-center">
+                        <td
+                          onClick={() => {
+                            setSelectedOffer(car);
+                            setModalShown(true);
+                          }}
+                        >
+                          {car.model}-{car.licenceNumber}
+                        </td>
+                      </tr>
+                    ))}
+                </td>
+
+                <td>{customer.firstName}</td>
+                <td>{customer.lastName}</td>
+                <td>{customer.address.street}</td>
+                <td>{customer.address.city}</td>
+                <td>{customer.address.zip}</td>
+                <td>{customer.address.voivodeship}</td>
+                <td>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      adminActionsDispatcher.deleteCustomer(customer);
+                    }}
+                  >
+                    Remove user
+                  </Button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
