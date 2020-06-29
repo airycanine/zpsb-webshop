463c826da4360486ba2875524bf337166f1cd409
qbavv <kubawalat@gmail.com>
2020-06-29 23:01:24 +0200

    endpoint changed to zpsb

--- src/consts/endpoints.ts
+++ src/consts/endpoints.ts
@@ -1,4 +1,5 @@
-export const API_ENDPOINT = "http://localhost:8080";
+// export const API_ENDPOINT = "http://localhost:8080";
+export const API_ENDPOINT = "https://zpsb-webshop-api.herokuapp.com";
 export const CUSTOMERS_POSTFIX = "/users";
 export const LOG_IN_ENDPOINT = "/api/auth/sign-in";
 export const REGISTER_ENDPOINT = "/api/auth/sign-up";
